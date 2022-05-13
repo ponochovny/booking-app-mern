c05f6a1569a5382a1b6f67bf0834f866ac2bfeb1
ponochovny <ponochovny@gmail.com>
2022-05-14 01:11:49 +0300

    hotels requests

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+
 const app = express()
 dotenv.config()
 
@@ -23,6 +29,15 @@ mongoose.connection.on('connected', () => {
 app.get('/', (req, res) => {
     res.send('hello first request!')
 })
+
+// middlewares
+
+app.use(express.json())
+
+app.use('/api/auth', authRoute)
+app.use('/api/users', usersRoute)
+app.use('/api/hotels', hotelsRoute)
+app.use('/api/rooms', roomsRoute)
 
 app.listen(8800, () => {
     connect()
