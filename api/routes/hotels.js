3d6e39f97277b86aa739a1d008283496d4814248
ponochovny <ponochovny@gmail.com>
2022-05-14 02:59:50 +0300

    created controllers for hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,64 +1,27 @@
 import express from 'express'
-import Hotel from '../models/Hotel.js'
-import { createError } from '../utils/error.js'
+import {
+	createHotel,
+	updateHotel,
+	deleteHotel,
+	getHotel,
+	getHotels,
+} from '../controllers/hotel.js'
 
 const router = express.Router()
 
 // CREATE
-router.post('/', async (req, res) => {
-	const newHotel = new Hotel(req.body)
+router.post('/', createHotel)
 
-	try {
-		const savedHotel = await newHotel.save()
-		res.status(200).json(savedHotel)
-	} catch (error) {
-		res.status(500).json(error)
-	}
-})
 // UPDATE
-router.put('/:id', async (req, res) => {
-	const newHotel = new Hotel(req.body)
+router.put('/:id', updateHotel)
 
-	try {
-		const updatedHotel = await Hotel.findByIdAndUpdate(
-			req.params.id,
-			{ $set: req.body },
-			{ new: true }
-		)
-		res.status(200).json(updatedHotel)
-	} catch (error) {
-		res.status(500).json(error)
-	}
-})
 // DELETE
-router.delete('/:id', async (req, res) => {
-	const newHotel = new Hotel(req.body)
+router.delete('/:id', deleteHotel)
 
-	try {
-		await Hotel.findByIdAndDelete(req.params.id)
-		res.status(200).json('Hotel has been deleted.')
-	} catch (error) {
-		res.status(500).json(error)
-	}
-})
 // GET
-router.get('/:id', async (req, res) => {
-	try {
-		const hotel = await Hotel.findById(req.params.id)
-		res.status(200).json(hotel)
-	} catch (error) {
-		res.status(500).json(error)
-	}
-})
+router.get('/:id', getHotel)
+
 // GET ALL
-router.get('/', async (req, res, next) => {
-	try {
-		const hotels = await Hotel.find()
-		res.status(200).json(hotels)
-	} catch (error) {
-		// res.status(500).json(error)
-		next(error)
-	}
-})
+router.get('/', getHotels)
 
 export default router
