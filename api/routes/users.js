a40079c5b50f3e58679ff14e4e228a6149f5da4f
ponochovny <ponochovny@gmail.com>
2022-05-14 04:27:33 +0300

    user/isAdmin check for update this account

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,12 +5,15 @@ import {
 	getUser,
 	getUsers,
 } from '../controllers/user.js'
-import { verifyToken } from '../utils/verifyToken.js'
+import { verifyToken, verifyUser } from '../utils/verifyToken.js'
 
 const router = express.Router()
 
 router.get('/checkauthentication', verifyToken, (req, res, next) => {
 	res.send('hello user, you are logged in')
+})
+router.get('/checkuser/:id', verifyUser, (req, res, next) => {
+	res.send('hello user, you are logged in and you can delete you account')
 })
 
 // UPDATE
