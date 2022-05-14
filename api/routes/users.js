2afcc990438edef0fca261dd64d700b798f26d1c
ponochovny <ponochovny@gmail.com>
2022-05-14 04:31:06 +0300

    user check is admin

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,7 +5,7 @@ import {
 	getUser,
 	getUsers,
 } from '../controllers/user.js'
-import { verifyToken, verifyUser } from '../utils/verifyToken.js'
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js'
 
 const router = express.Router()
 
@@ -13,7 +13,10 @@ router.get('/checkauthentication', verifyToken, (req, res, next) => {
 	res.send('hello user, you are logged in')
 })
 router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-	res.send('hello user, you are logged in and you can delete you account')
+	res.send('hello user, you are logged in and you can delete your account')
+})
+router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
+	res.send('hello admin, you are logged in and you can delete all accounts')
 })
 
 // UPDATE
