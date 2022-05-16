59f02cf2cb2f401c892c2a23296f01b859a09f73
ponochovny <ponochovny@gmail.com>
2022-05-16 03:34:12 +0300

    featured properties list

---
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,66 @@
+import './FeaturedProperties.scss'
+
+const FeaturedProperties = () => {
+	return (
+		<div className="fp">
+			<div className="fpItem">
+				<img
+					className="fpImg"
+					src="https://cf.bstatic.com/xdata/images/hotel/max300/313635032.webp?k=f2afe5153cc930d8b95a663a44554e6d9e70bfbdb50e440e27bfede174b256fb&o="
+					alt=""
+				/>
+				<span className="fpName">The Grand Sandestin</span>
+				<span className="fpCity">Madrid</span>
+				<span className="fpPrice">Starting from $120</span>
+				<div className="fpRating">
+					<button>8.9</button>
+					<span>Excellent</span>
+				</div>
+			</div>
+			<div className="fpItem">
+				<img
+					className="fpImg"
+					src="https://cf.bstatic.com/xdata/images/hotel/max300/313635032.webp?k=f2afe5153cc930d8b95a663a44554e6d9e70bfbdb50e440e27bfede174b256fb&o="
+					alt=""
+				/>
+				<span className="fpName">The Grand Sandestin</span>
+				<span className="fpCity">Madrid</span>
+				<span className="fpPrice">Starting from $120</span>
+				<div className="fpRating">
+					<button>8.9</button>
+					<span>Excellent</span>
+				</div>
+			</div>
+			<div className="fpItem">
+				<img
+					className="fpImg"
+					src="https://cf.bstatic.com/xdata/images/hotel/max300/313635032.webp?k=f2afe5153cc930d8b95a663a44554e6d9e70bfbdb50e440e27bfede174b256fb&o="
+					alt=""
+				/>
+				<span className="fpName">The Grand Sandestin</span>
+				<span className="fpCity">Madrid</span>
+				<span className="fpPrice">Starting from $120</span>
+				<div className="fpRating">
+					<button>8.9</button>
+					<span>Excellent</span>
+				</div>
+			</div>
+			<div className="fpItem">
+				<img
+					className="fpImg"
+					src="https://cf.bstatic.com/xdata/images/hotel/max300/313635032.webp?k=f2afe5153cc930d8b95a663a44554e6d9e70bfbdb50e440e27bfede174b256fb&o="
+					alt=""
+				/>
+				<span className="fpName">The Grand Sandestin</span>
+				<span className="fpCity">Madrid</span>
+				<span className="fpPrice">Starting from $120</span>
+				<div className="fpRating">
+					<button>8.9</button>
+					<span>Excellent</span>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default FeaturedProperties
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,21 +1,24 @@
-import Featured from '../../components/featured/Featured'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import PropertyList from '../../components/propertyList/PropertyList'
 import './Home.scss'
 
 const Home = () => {
-  return (
-    <div>
-      <Navbar />
-      <Header />
-      <div className="homeContainer">
-        <Featured />
-        <h1 className="homeTitle">Browse by property type</h1>
-        <PropertyList />
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<Header />
+			<div className="homeContainer">
+				<Featured />
+				<h1 className="homeTitle">Browse by property type</h1>
+				<PropertyList />
+				<h1 className="homeTitle">Homes guests love</h1>
+				<FeaturedProperties />
+			</div>
+		</div>
+	)
 }
 
 export default Home
