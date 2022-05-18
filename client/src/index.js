8ba5418bb9cbbf9c67c9b37d3e2af514aca60fed
ponochovny <ponochovny@gmail.com>
2022-05-18 06:27:39 +0300

    created auth context

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { SearchContextProvider } from './context/SearchContext'
+import { AuthContextProvider } from './context/AuthContext'
 import './normalize.scss'
 import './index.scss'
 
@@ -11,8 +12,10 @@ import 'react-date-range/dist/theme/default.css' // theme css file
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<SearchContextProvider>
-			<App />
-		</SearchContextProvider>
+		<AuthContextProvider>
+			<SearchContextProvider>
+				<App />
+			</SearchContextProvider>
+		</AuthContextProvider>
 	</React.StrictMode>
 )
