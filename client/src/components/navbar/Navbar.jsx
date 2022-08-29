bfd171f0311872042434042d189b9726b29928c7
ponochovny <ponochovny@gmail.com>
2022-08-29 18:22:00 +0300

    extended login functionality

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,23 +1,61 @@
 import { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
 import './Navbar.scss'
 
 const Navbar = () => {
-	const { user } = useContext(AuthContext)
+	const { user, dispatch } = useContext(AuthContext)
+	const navigate = useNavigate()
+
+	const handleLogOut = () => {
+		dispatch({ type: 'LOGOUT' })
+	}
 
 	return (
-		<div className="navbar">
-			<div className="navContainer">
-				<Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>
-					<span className="logo">ponochovnybooking</span>
+		<div className='navbar'>
+			<div className='navContainer'>
+				<Link to='/' style={{ color: 'inherit', textDecoration: 'none' }}>
+					<span className='logo'>ponochovnybooking</span>
 				</Link>
 				{user ? (
-					user.username
+					<div className='navContainer__username'>
+						{user.username}
+						<button
+							className='btn btn_link btn_link_white'
+							onClick={handleLogOut}
+						>
+							<svg
+								xmlns='http://www.w3.org/2000/svg'
+								width='16'
+								height='16'
+								fill='currentColor'
+								class='bi bi-box-arrow-right'
+								viewBox='0 0 16 16'
+							>
+								<path
+									fill-rule='evenodd'
+									d='M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z'
+								/>
+								<path
+									fill-rule='evenodd'
+									d='M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z'
+								/>
+							</svg>
+						</button>
+					</div>
 				) : (
-					<div className="navItems">
-						<button className="navButton">Register</button>
-						<button className="navButton">Login</button>
+					<div className='navItems'>
+						<button
+							className='navButton'
+							onClick={() =>
+								navigate('/login', { state: { isRegister: true } })
+							}
+						>
+							Register
+						</button>
+						<button className='navButton' onClick={() => navigate('/login')}>
+							Login
+						</button>
 					</div>
 				)}
 			</div>
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,14 +1,20 @@
 import axios from 'axios'
+import { useEffect } from 'react'
 import { useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
 import './Login.scss'
 
 const Login = () => {
+	const [isLogin, setIsLogin] = useState(true)
 	const [credentials, setCredentials] = useState({
 		username: undefined,
 		password: undefined,
+		email: undefined,
 	})
+
+	const location = useLocation()
+	const isRegister = location.state?.isRegister
 
 	const { loading, error, dispatch } = useContext(AuthContext)
 
@@ -18,11 +24,19 @@ const Login = () => {
 		setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }))
 	}
 
+	const loginCredentials = ({ email, ...items }) => {
+		return { ...items }
+	}
+
 	const handleClick = async (e) => {
 		e.preventDefault()
+
 		dispatch({ type: 'LOGIN_START' })
 		try {
-			const res = await axios.post('/auth/login', credentials)
+			if (!isLogin) {
+				await axios.post(`/auth/register`, credentials)
+			}
+			const res = await axios.post(`/auth/login`, loginCredentials(credentials))
 			dispatch({ type: 'LOGIN_SUCCESS', payload: res.data })
 			navigate('/')
 		} catch (error) {
@@ -30,27 +44,55 @@ const Login = () => {
 		}
 	}
 
+	useEffect(() => {
+		setIsLogin(!!isRegister ? false : true)
+	}, [])
+
 	return (
-		<div className="login">
-			<div className="lContainer">
+		<div className='login'>
+			<div className='lContainer'>
 				<input
-					type="text"
-					placeholder="username"
-					id="username"
+					type='text'
+					placeholder='username'
+					id='username'
 					onChange={handleChange}
-					className="lInput"
+					className='lInput'
 				/>
+				{!isLogin && (
+					<input
+						type='text'
+						placeholder='email'
+						id='email'
+						onChange={handleChange}
+						className='lInput'
+					/>
+				)}
 				<input
-					type="password"
-					placeholder="password"
-					id="password"
+					type='password'
+					placeholder='password'
+					id='password'
 					onChange={handleChange}
-					className="lInput"
+					className='lInput'
 				/>
-				<button disabled={loading} onClick={handleClick} className="lButton">
-					Login
+				<button disabled={loading} onClick={handleClick} className='lButton'>
+					{isLogin ? 'Login' : 'Register'}
 				</button>
-				{error && <span>{error.message}</span>}
+				{!isLogin ? (
+					<div>
+						Already have an account?
+						<button className='btn btn_link' onClick={() => setIsLogin(true)}>
+							Log In
+						</button>
+					</div>
+				) : (
+					<div>
+						Want to create an account?
+						<button className='btn btn_link' onClick={() => setIsLogin(false)}>
+							Register
+						</button>
+					</div>
+				)}
+				{error && <span style={{ color: 'red' }}>{error.message}</span>}
 			</div>
 		</div>
 	)
